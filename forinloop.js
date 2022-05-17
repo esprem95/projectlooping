c356e5909e7dc5ddee4280ffc1fcdc429eedbfbb
esprem95 <105243906+esprem95@users.noreply.github.com>
2022-05-18 00:24:30 +0530

    Add files via upload

---
+++ forinloop.js
@@ -0,0 +1,15 @@
+// for in loop concept
+
+let person={
+
+    name:"premkumar",
+    age:"25",
+    gender:"male",
+
+}
+for(let detail in person){
+
+    let details= (person[detail]);
+
+    console.log(detail +": ",details);
+} 
